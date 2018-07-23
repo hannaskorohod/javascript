c9818f92d26c1fac9de203210d438bf2518bf6c2
Hanna Skorohod <hanna.skorokhod@gmail.com>
2018-07-23 12:47:08 +0300

    Update 1_2.js

--- easy/1_2.js
+++ easy/1_2.js
@@ -1,11 +1,10 @@
 
-function myNumbers(numbers){
-	var emptyArr = [];
-	emptyArr.max = Math.max(...numbers);
-	emptyArr.min = Math.min(...numbers);
-	return emptyArr;
-};
-	var numbers = [3,5,767,32,7,9];
-	console.log(myNumbers(numbers));
+var myArr = [1,5,23,4,2,5,6];
+
+function numbers(){
+	console.log(Math.max(...myArr));
+	console.log(Math.min(...myArr));
+}
+numbers();
 
 
