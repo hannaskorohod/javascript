e9c0160b58eea719ae63a096027bc0813457a3cc
Hanna Skorohod <hanna.skorokhod@gmail.com>
2018-07-23 12:56:19 +0300

    Update 1_3.js

--- easy/1_3.js
+++ easy/1_3.js
@@ -1,8 +1,8 @@
 //1.3) Write a function that returns average number of values. Do not use Math. For i.e. [1,4,2] should return [2.33].
 
-function myNumber(a, b, c){
-	var sum = a + b + c;
-	var averNum = sum / 3;
-	return averNum.toFixed(2);
-};
-myNumber(1,4,2);
+var numbers = function (a, b, c){
+	var culc = (a + b + c) / 3;
+	return culc.toFixed(2);
+}
+
+numbers(1,4,2);
