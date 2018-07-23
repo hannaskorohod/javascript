7d9f50f35cc23da369d49eec591b16eb905f135f
Hanna Skorohod <hanna.skorokhod@gmail.com>
2018-07-23 12:08:16 +0300

    Update 1_1.js

--- easy/1_1.js
+++ easy/1_1.js
@@ -1,12 +1,5 @@
-//Write a function that returns only pair numbers. For i.e. input [1,5,23,4,2,5,6] should return [4,2,6].
-var arrNum = function (arr){
-var arr1 =[];
-for (var n in arr){
-if (arr[n] % 2 == 0){
-arr1.push(arr[n]);
-		}
-};
-return arr1;
-};
-var arr = [1,4,6,84,73,8];
-console.log(arrNum(arr));
+var myArr = [1,5,23,4,2,5,6];
+
+var result = myArr.filter(arr1 => arr1%2===0)
+
+console.log(result);
