59ae15bd5fe7ea0b4c467d02511b386215422076
Hanna Skorohod <hanna.skorokhod@gmail.com>
2018-07-23 12:17:14 +0300

    Update 1_1.js

--- easy/1_1.js
+++ easy/1_1.js
@@ -1,5 +1,8 @@
 var myArr = [1,5,23,4,2,5,6];
 
-var result = myArr.filter(arr1 => arr1%2===0)
+function pairNumber(){
 
+var result = myArr.filter(arr1 => arr1%2===0);
 console.log(result);
+}
+pairNumber();
